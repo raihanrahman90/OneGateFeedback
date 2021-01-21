50bfc3d398f4df053049a49d9a58470bc037abc1
re-beat <raihanr090@gmail.com>
2021-01-21 13:20:24 +0800

    ganti password

--- action/kembali.php
+++ action/kembali.php
@@ -4,7 +4,7 @@ include('../koneksi.php');
 $id = $koneksi-> real_escape_string($_POST['id']);
 $id_akun = $_SESSION['id_akun'];
 $keterangan = $koneksi-> real_escape_string($_POST['keterangan']);
-$data = mysqli_query($koneksi, "UPDATE tb_aduan SET  status ='Returned', id_unit=NULL WHERE id_aduan ='$id'") or die(mysqli_error($koneksi));
+$data = mysqli_query($koneksi, "UPDATE tb_aduan SET  status ='Returned', id_unit=NULL, nama_departemen=NULL, nama_unit=NULL WHERE id_aduan ='$id'") or die(mysqli_error($koneksi));
 $data1 = mysqli_query($koneksi, "INSERT INTO tb_progress VALUES(0,$id_akun,$id,'Dikembalikan ke cs dengan keterangan $keterangan',NULL,now())") or die(mysqli_error($koneksi));
 include('../pesan/kembali.php');
 header("location:../Admin");
