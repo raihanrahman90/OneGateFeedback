d0283c6c54c414d74e2ebb299d8e6df5f9d2d7c9
re-beat <raihanr090@gmail.com>
2021-01-22 13:28:12 +0800

    perbaikan grafik

--- action/datasets_penilaian.php
+++ action/datasets_penilaian.php
@@ -45,7 +45,7 @@
     	}
 	} else {
 	    if($rentang=='Tahun'){
-        	$data = mysqli_query($koneksi,"SELECT count(id_aduan), jenis, year(date(waktu_kirim)) FROM tb_aduan 
+        	$data = mysqli_query($koneksi,"SELECT count(tb_aduan.id_aduan), jenis, year(date(waktu_kirim)) FROM tb_aduan 
                                             inner join tb_penilaian on tb_penilaian.id_aduan = tb_aduan.id_aduan 
                                             where year(date(waktu_kirim))>=year(date('".$from."')) and year(date(waktu_kirim)) <= year(date('".$to."')) group by penilaian, year(date(waktu_kirim)) order by waktu_kirim");
         	foreach($data as $row){
@@ -55,7 +55,7 @@
         	    }
         	}
     	} else if($rentang =='Bulan'){
-        	$data = mysqli_query($koneksi,"SELECT count(id_aduan), jenis, DATE_FORMAT(date(waktu_kirim),'%Y-%m') FROM tb_aduan  
+        	$data = mysqli_query($koneksi,"SELECT count(tb_aduan.id_aduan), jenis, DATE_FORMAT(date(waktu_kirim),'%Y-%m') FROM tb_aduan  
                                             inner join tb_penilaian on tb_penilaian.id_aduan = tb_aduan.id_aduan 
                                             where date(waktu_kirim)>=date('".$from."') and date(waktu_kirim) <= date('".$to."') group by penilaian, DATE_FORMAT(date(waktu_kirim),'%Y%m') order by waktu_kirim") or die(mysqli_error($koneksi));
         	foreach($data as $row){
@@ -65,7 +65,7 @@
         	    }
         	}
     	} else {
-    	    $data = mysqli_query($koneksi,"SELECT count(id_aduan), jenis, DATE_FORMAT(date(waktu_kirim),'%d-%m-%Y') FROM tb_aduan  
+    	    $data = mysqli_query($koneksi,"SELECT count(tb_aduan.id_aduan), jenis, DATE_FORMAT(date(waktu_kirim),'%d-%m-%Y') FROM tb_aduan  
                                             inner join tb_penilaian on tb_penilaian.id_aduan = tb_aduan.id_aduan 
                                             where date(waktu_kirim)>=date('".$from."') and date(waktu_kirim) <= date('".$to."') group by penilaian, DATE_FORMAT(date(waktu_kirim),'%d-%m-%Y') order by waktu_kirim");
         	foreach($data as $row){
