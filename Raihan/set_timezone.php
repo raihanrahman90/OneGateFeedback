15d03d2f6b8d422ad54555674d2abc43ece24906
re-beat <raihanr090@gmail.com>
2021-07-01 16:26:37 +0800

    ganti set_timezone.php

--- Raihan/set_timezone.php
+++ Raihan/set_timezone.php
@@ -1,6 +1,7 @@
 <?php
     include '../koneksi.php';
-    mysqli_query($koneksi, "SET GLOBAL time_zone = '+8:00'");   
+    $timezone = $_GET['timezone'];
+    mysqli_query($koneksi, "SET GLOBAL time_zone = '+$timezone:00'");   
     $query = mysqli_query($koneksi, "select now() as data");
     while($data = mysqli_fetch_array($query)){
         echo $data['data'];
